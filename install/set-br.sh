40cbfdfee12e7127a4c79f75adc9ed6331c36a8c
PEYX <peyxdev@gmail.com>
2026-08-11 09:20:28 +0700

    Add files via upload

---
+++ install/set-br.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REPO="https://raw.githubusercontent.com/PeyxDev/esce/main/"
+apt install rclone
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "${REPO}install/rclone.conf"
+git clone  https://github.com/casper9/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+
+rm -f /root/set-br.sh
